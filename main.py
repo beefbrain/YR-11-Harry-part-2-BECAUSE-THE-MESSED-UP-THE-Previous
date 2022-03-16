011ccf74c4b9affe971cd3ac6351dcfcd531e422
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-16 02:45:35 +0000

    This is my first attempt of the introduction to my quiz

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name = input("What is your name?")
-print
+#intro
+name = input("What is your name? ")
+print("Hello {}".format(name))
+print ("Welcome to this harry potter quiz! Harrp potter is a seris written by JK Rowling. you will be asked to enter the number that equals your answer, at the end of this quiz you will find out who is your hogwarts best friend")
