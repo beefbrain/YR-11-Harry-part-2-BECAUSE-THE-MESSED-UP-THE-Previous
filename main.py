4a89a19eff4da1e18f1eb88893fd2081d2cb4b9e
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-19 06:07:14 +0000

    I debugged the sets, adding capital letters and comma's and spaces. I also added a function as a short cut as all the keys in the dictionary have the same value

--- main.py
+++ main.py
@@ -1,74 +1,74 @@
 #sets and dictionaries 
 
-#dictionary of all the characters
-Char_score = {"Harry Potter":0, "Hermioni Granger": 0""""""}
+#dictionary of all the characters all characters have the same value of 0 so i found a finction that is a short cut for this
+char_score = {}.fromkeys(["Hermioni Granger", "Harry Potter", "Ron Weasley", "Draco Malfoy", "Neville Longbottom", "Luna Lovegood", "Cedric Diggory", "Fred and George Weasley", "Ginny Weasley" ], 0)
 
 #Who would you want to be friends with?
-bad_boy_girl = {"Harry Potter", "Draco malfoy", "Ginny weasley"} 
-gd_influ = {"Hermioni Granger", "Neville Longbottom", "Ginny weasley"} 
-reserved = {"neville longbottom", "luna lovegood", "Ginny weasley"} 
+bad_boy_girl = {"Harry Potter", "Draco Malfoy", "Ginny Weasley"} 
+gd_influ = {"Hermioni Granger", "Neville Longbottom", "Ginny Weasley"} 
+reserved = {"Neville Longbottom", "Luna Lovegood", "Ginny Weasley"} 
 
 #matching opperation will be applied for thsi question here
 
 #Which one of these would you like  in a friend would you like?
-funny ={"Ron weasley", "Draco malfoy", "fred and george weasley", "Ginny weasley"}
-popular = {"Harry Potter", "Draco malfoy", "Cedric Diggory"}
+funny ={"Ron Weasley", "Draco Malfoy", "Fred and George Weasley", "Ginny Weasley"}
+popular = {"Harry Potter", "Draco Malfoy", "Cedric Diggory"}
 brave= {"Harry Potter", "Cedric Diggory"}
 
 #Pick another 
-courageous = {"Ron Weasley",   "neville longbottom"}
-intelligence = { "Hermioni Granger", "Draco malfoy Cedric Diggory", "Ginny Weasley". 
-positivity = { "luna lovegood", "fred and george Weasley"}
+courageous = {"Ron Weasley", "Neville Longbottom"}
+intelligence = { "Hermioni Granger", "Draco Malfoy Cedric Diggory", "Ginny Weasley". 
+positivity = { "Luna Lovegood", "Fred and George Weasley"}
 
 #matching opperation will be applied for thsi question here
 
 #What trait would you love in your friend
-loyal = {"Hermioni Granger", "Harry Potter", "Ron Weasley", "neville longbottom", "luna lovegood", "Cedric Diggory", "fred and george Weasley", "Ginny Weasley"}
+loyal = {"Hermioni Granger", "Harry Potter", "Ron Weasley", "Neville Longbottom", "Luna Lovegood", "Cedric Diggory", "Fred and George Weasley", "Ginny Weasley"}
 sacrifice  = {"Hermioni Granger",   "Harry Potter", "Cedric Diggory"}
-accept = {"Harry Potter", "luna lovegood", "Cedric Diggory", "Ginny weasley"} 
-kind = {"neville longbottom", "Cedric Diggory", "Luna Lovegood"}
-helpful = {"Hermioni Granger", "neville longbottom", "Cedric Diggory", "Ginny weasley"}
+accept = {"Harry Potter", "Luna Lovegood", "Cedric Diggory", "Ginny Weasley"} 
+kind = {"Neville Longbottom", "Cedric Diggory", "Luna Lovegood"}
+helpful = {"Hermioni Granger", "Neville Longbottom", "Cedric Diggory", "Ginny Weasley"}
 
 #matching opperation will be applied for thsi question here
 
 #Pick a hogwarts subject:
-Def_dark_art = {"Ron weasley", "Harry potter", "Draco malfoy"}
-Herb = {"neville longbottom","fred and george weasley"}
-Care_magic_cre = {"luna lovegood"}
-Fly_class= {"Ginny weasley", "Cedric Diggory", "fred and george weasley"}
-Arith = {"Hermioni Granger", "Harry potter", "Draco malfoy"}
+Def_dark_art = {"Ron Weasley", "Harry Potter", "Draco Malfoy"}
+Herb = {"Neville Longbottom", "Fred and George Weasley"}
+Care_magic_cre = {"Luna Lovegood"}
+Fly_class= {"Ginny Weasley", "Cedric Diggory", "Fred and George Weasley"}
+Arith = {"Hermioni Granger", "Harry Potter", "Draco Malfoy"}
 Charms = {"Cedric Diggory"}
 
 #matching opperation will be applied for thsi question here
 
 #What is your hobby
-Play_sport = {"Ginny weasley" "cedric diggory", "fred and george weasley", "Harry potter", "Draco malfoy"}
-vid_brd_game = {"ron weasley"} 
-art = {"luna lovegood"}
+Play_sport = {"Ginny Weasley" "Cedric Diggory", "Fred and George Weasley", "Harry Potter", "Draco Malfoy"}
+vid_brd_game = {"Ron Weasley"} 
+art = {"Luna Lovegood"}
 Reading = {"Hermioni Granger"}
-Cooking = {"neville longbottom"}
-Gardening = {"neville longbottom", "luna lovegood"}
-joke_memes {"fred and george weasley"}
+Cooking = {"Neville Longbottom"}
+Gardening = {"Neville Longbottom", "Luna Lovegood"}
+joke_memes {"Fred and George Weasley"}
 
 #matching opperation will be applied for thsi question here
 
 #How are you spending your weekend
-Read_book = {"Hermioni Granger",  "neville longbottom"}
-myself = {"luna lovegood"}
-Binge = "ron weasley" 
-friends = {"Harry potter", "cedric diggory"}
-family = {"Ginny weasley", "fred and george weasley", "Draco malfoy"}
+Read_book = {"Hermioni Granger",  "Neville Longbottom"}
+myself = {"Luna Lovegood"}
+Binge = "Ron Weasley" 
+friends = {"Harry Potter", "Cedric Diggory"}
+family = {"Ginny Weasley", "Fred and George Weasley", "Draco Malfoy"}
 
 #matching opperation will be applied for thsi question here
 
 #What is a trait you cannot tolerate in a person
-oversensitive = {"neville longbottom"}
-Insensitive = {"Hermioni Granger", "ron weasley", "luna lovegood", "fred and george weasley", "Draco malfoy"}
-Arrogant = {"ron weasley",  "Draco malfoy"}
-Opinionated = {"Hermioni Granger", "Harry potter", "luna lovegood", "Ginny weasley"}
-before_think = {"Harry potter", "fred and george weasley", "ron weasley"} 
-Pessimist = {"draco malfoy", "ron weasley"}
-impatient = {"Hermioni Granger", "Ginny weasley"}
+oversensitive = {"Neville Longbottom"}
+Insensitive = {"Hermioni Granger", "Ron Weasley", "Luna Lovegood", "Fred and George Weasley", "Draco Malfoy"}
+Arrogant = {"Ron Weasley",  "Draco Malfoy"}
+Opinionated = {"Hermioni Granger", "Harry Potter", "Luna Lovegood", "Ginny Weasley"}
+before_think = {"Harry Potter", "Fred and George Weasley", "Ron Weasley", "Cedric Diggory"} 
+Pessimist = {"Draco Malfoy", "Ron Weasley"}
+impatient = {"Hermioni Granger", "Ginny Weasley"}
 
 #matching opperation will be applied for thsi question here
 
@@ -76,5 +76,5 @@ impatient = {"Hermioni Granger", "Ginny weasley"}
 #REUSING THE SETS ABOVE
 
 #Would you rather have a friend who is very…
-Boring = {"neville longbottom"} 
-Odd = {"luna lovegood"} 
+Boring = {"Neville Longbottom"} 
+Odd = {"Luna Lovegood"} 
