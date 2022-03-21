989c598e18f7c5804426d55a0e87913a6f9f2fb7
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-21 06:11:07 +0000

    I tried a different method for the input condition. then I linked the dictionary and sets

--- main.py
+++ main.py
@@ -11,15 +11,32 @@ print ("Someone who is a good influence so they can encurage me to stay on track
 print ("Enter 1")
 print ("Someone a bit quiet and down to earth, they are very relaxing to hang out with")
 print ("Enter 2")
+#what id id to make this work was made my while and if statememnt more specific and added the input in the loop which i didn't do before.
 ans = int(input("Enter your option here..."))
 while ans > 2:
-  if ans > 2:
-    print("That is not an option, try again")
-    ans = int(input("Enter your option here..."))
-  else:
+  print("That is not an option, try again")
+  ans = int(input("Enter your option here..."))
+  if ans <=2:
    break
   
 bad_boy_girl = {"Harry Potter", "Draco Malfoy", "Ginny Weasley"} 
 gd_influ = {"Hermioni Granger", "Neville Longbottom", "Ginny Weasley"} 
 quiet = {"Neville Longbottom", "Luna Lovegood", "Cedric Diggory"} 
+ans_set = {}
 
+if ans == 0:
+    ans_set = bad_boy_girl
+elif ans == 1:
+    ans_set = gd_influ
+elif ans == 2:
+  ans_set = quiet
+
+# the keys in the the dictionary char_score is the same to the value of all of the sets. i need to use a for loop to link them as the for loop will check the dictionary keys one after the other.
+
+for character in ans_set:
+#character holds the values in ans_set
+  #print(character)
+#i am printing to test if characters holds the correct values
+  char_score[character] +=1
+print (char_score)
+#printing to test if the vaules are added up correctly 
