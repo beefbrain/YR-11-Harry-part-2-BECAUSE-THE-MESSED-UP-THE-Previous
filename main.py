8f2e7ecbb234b7b2345449ac602dc036f22426d6
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-22 09:33:15 +0000

    IN this version, if alphabet is not given at the start, it will ask to do aplhabet,if user still doesn't the code crashes.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #intro
 name = input("what's your name? (press enter after writing your name) ")
 print("ʕ•́ᴥ•̀ʔっ♡ Hello {}".format(name)) 
-print ("welcome to this harry potter quiz ʕ•́ᴥ•̀ʔっ♡ ")
-print ("Harry potter is a book series written by auther JK Rowlings.She wrote a total of 7 books but this quiz is based on 6 of them")
-print("   ")
-print ("          .。･:*:･(✿ ◕ 3 ◕ ) ❤ ( ◕ ε ◕ ✿ )･:*:･。.")
-print("   ")
-print ("You'll be instructed to press a number that equals your option then press enter right after you entered it, at the end of the quiz, you will find out who your hogwarts best friend or people in your freind group are!")
+if not name.isalpha():
+      print("Please enter only alphabetical characters for your name.")
+
+elif name.isalpha():
+  print ("welcome to this harry potter quiz ʕ•́ᴥ•̀ʔっ♡ ")
+  print ("Harry potter is a book series written by auther JK Rowlings.She wrote a total of 7 books but this quiz is based on 6 of them")
+  print("   ")
+  print ("          .。･:*:･(✿ ◕ 3 ◕ ) ❤ ( ◕ ε ◕ ✿ )･:*:･。.")
+  print("   ")
+  print ("You'll be instructed to press a number that equals your option then press enter right after you entered it, at the end of the quiz, you will find out who your hogwarts best friend or people in your freind group are!")
