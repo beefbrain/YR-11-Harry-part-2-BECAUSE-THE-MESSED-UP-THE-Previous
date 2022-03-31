62a6aab8c28925026769b14b3d725586a2880ae0
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-31 08:15:57 +0000

    Made the introduction more aesthetic and more usable

--- main.py
+++ main.py
@@ -1,80 +1,29 @@
-#sets and dictionaries 
+#intro
+name = str(input("what's your name? (press enter after writing your name) "))
+print (" ")
 
-#dictionary of all the characters all characters have the same value of 0 so i found a finction that is a short cut for this
-char_score = {}.fromkeys(["Hermioni Granger", "Harry Potter", "Ron Weasley", "Draco Malfoy", "Neville Longbottom", "Luna Lovegood", "Cedric Diggory", "Fred and George Weasley", "Ginny Weasley" ], 0)
+non_caps = 0 
+  
+while non_caps ==0:
+  if not name.isalpha():
+    print ("*******************************************************")
+    print("Please enter only alphabetical characters for your name.")
+    print ("*******************************************************")
+    print("  ")
+    name = str(input("what's your name? (press enter after writing your name) "))
+    print(" ")
+    
+    non_caps = 0
+  if name.isalpha():
+    non_caps = 1
 
-#Who would you want to be friends with?
-bad_boy_girl = {"Harry Potter", "Draco Malfoy", "Ginny Weasley"} 
-gd_influ = {"Hermioni Granger", "Neville Longbottom", "Ginny Weasley"} 
-reserved = {"Neville Longbottom", "Luna Lovegood", "Ginny Weasley"} 
 
-#matching opperation will be applied for thsi question here
 
-#Which one of these would you like  in a friend would you like?
-funny ={"Ron Weasley", "Draco Malfoy", "Fred and George Weasley", "Ginny Weasley"}
-popular = {"Harry Potter", "Draco Malfoy", "Cedric Diggory"}
-brave= {"Harry Potter", "Cedric Diggory"}
-
-#Pick another 
-courageous = {"Ron Weasley", "Neville Longbottom"}
-intelligence = { "Hermioni Granger", "Draco Malfoy Cedric Diggory", "Ginny Weasley"}
-positivity = { "Luna Lovegood", "Fred and George Weasley"}
-
-#matching opperation will be applied for thsi question here
-
-#What trait would you love in your friend
-loyal = {"Hermioni Granger", "Harry Potter", "Ron Weasley", "Neville Longbottom", "Luna Lovegood", "Cedric Diggory", "Fred and George Weasley", "Ginny Weasley"}
-sacrifice  = {"Hermioni Granger",   "Harry Potter", "Cedric Diggory"}
-accept = {"Harry Potter", "Luna Lovegood", "Cedric Diggory", "Ginny Weasley"} 
-kind = {"Neville Longbottom", "Cedric Diggory", "Luna Lovegood"}
-helpful = {"Hermioni Granger", "Neville Longbottom", "Cedric Diggory", "Ginny Weasley"}
-
-#matching opperation will be applied for thsi question here
-
-#Pick a hogwarts subject:
-Def_dark_art = {"Ron Weasley", "Harry Potter", "Draco Malfoy"}
-Herb = {"Neville Longbottom", "Fred and George Weasley"}
-Care_magic_cre = {"Luna Lovegood"}
-Fly_class= {"Ginny Weasley", "Cedric Diggory", "Fred and George Weasley"}
-Arith = {"Hermioni Granger", "Harry Potter", "Draco Malfoy"}
-Charms = {"Cedric Diggory"}
-
-#matching opperation will be applied for thsi question here
-
-#What is your hobby
-Play_sport = {"Ginny Weasley" "Cedric Diggory", "Fred and George Weasley", "Harry Potter", "Draco Malfoy"}
-vid_brd_game = {"Ron Weasley"} 
-art = {"Luna Lovegood"}
-Reading = {"Hermioni Granger"}
-Cooking = {"Neville Longbottom"}
-Gardening = {"Neville Longbottom", "Luna Lovegood"}
-joke_memes = {"Fred and George Weasley"}
-
-#matching opperation will be applied for thsi question here
-
-#How are you spending your weekend
-Read_book = {"Hermioni Granger",  "Neville Longbottom"}
-myself = {"Luna Lovegood"}
-Binge = "Ron Weasley" 
-friends = {"Harry Potter", "Cedric Diggory"}
-family = {"Ginny Weasley", "Fred and George Weasley", "Draco Malfoy"}
-
-#matching opperation will be applied for thsi question here
-
-#What is a trait you cannot tolerate in a person
-oversensitive = {"Neville Longbottom"}
-Insensitive = {"Hermioni Granger", "Ron Weasley", "Luna Lovegood", "Fred and George Weasley", "Draco Malfoy"}
-Arrogant = {"Ron Weasley",  "Draco Malfoy"}
-Opinionated = {"Hermioni Granger", "Harry Potter", "Luna Lovegood", "Ginny Weasley"}
-before_think = {"Harry Potter", "Fred and George Weasley", "Ron Weasley", "Cedric Diggory"} 
-Pessimist = {"Draco Malfoy", "Ron Weasley"}
-impatient = {"Hermioni Granger", "Ginny Weasley"}
-
-#matching opperation will be applied for thsi question here
-
-#What’s an annoying trate u don’t mind tolerating.
-#REUSING THE SETS ABOVE
-
-#Would you rather have a friend who is very…
-Boring = {"Neville Longbottom"} 
-Odd = {"Luna Lovegood"} 
+print (" ")
+print("ʕ•́ᴥ•̀ʔっ♡ Hello {}".format(name)) 
+print ("welcome to this harry potter quiz ʕ•́ᴥ•̀ʔっ♡ ")
+print ("Harry potter is a book series written by auther JK Rowlings.She wrote a total of 7 books but this quiz is based on 6 of them")
+print("   ")
+print ("          .。･:*:･(✿ ◕ 3 ◕ ) ❤ ( ◕ ε ◕ ✿ )･:*:･。.")
+print("   ")
+print ("You'll be instructed to press a number that equals your option then press enter right after you entered it, at the end of the quiz, you will find out who your hogwarts best friend or the people in your freind group are!")
